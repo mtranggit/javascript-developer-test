47462d320ee1f9c8aafd0beab49e002a65f74b36
Michael Trang <michael.trang@hotmail.com>
2026-01-29 23:57:16 +0000

    Implemented `getArnieQuotes`

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,19 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const promises = urls.map((url) => httpGet(url));
+  const responseData = await Promise.allSettled(promises);
+
+  const results = responseData.map((response) => {
+    const responseBody = JSON.parse(response.value.body);
+    if (response.value.status === 200) {
+      return {['Arnie Quote'] : responseBody.message};
+    } else {
+      return {['FAILURE'] : responseBody.message};
+    }
+  });
+
+  return results;
 };
 
 module.exports = {
